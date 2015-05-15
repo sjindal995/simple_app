78d7d67a4b460c6826ce7aa9a6d50b035e3122fa
Sahil Jindal <sjindal995@gmail.com>
2015-05-15 16:37:37 +0530

    transition: splash to menu screen

--- MyFirst/app/src/main/java/com/example/sahiljindal/myfirst/MainActivity.java
+++ MyFirst/app/src/main/java/com/example/sahiljindal/myfirst/MainActivity.java
@@ -1,11 +1,14 @@
 package com.example.sahiljindal.myfirst;
 
+import android.content.Intent;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.animation.Animation;
 import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
+import android.widget.TableLayout;
 import android.widget.TextView;
 
 
@@ -15,19 +18,42 @@ public class MainActivity extends ActionBarActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        TextView logo1 = (TextView) findViewById(R.id.textView2);
-        Animation fade1 = AnimationUtils.loadAnimation(this,R.anim.fade_in);
+//        TextView logo1 = (TextView) findViewById(R.id.textView2);
+        final TextView logo2 = (TextView) findViewById(R.id.textView2);
+        final TextView logo1 = (TextView) findViewById(R.id.textView2);
+        final Animation fade1 = AnimationUtils.loadAnimation(this,R.anim.fade_in);
+        TableLayout pics = (TableLayout) findViewById(R.id.TableLayout);
+        Animation rotation = AnimationUtils.loadAnimation(this,R.anim.rotate);
         logo1.startAnimation(fade1);
-//        TextView logo2 = (TextView) findViewById(R.id.textView2);
-//        Animation fade2 = AnimationUtils.loadAnimation(this,R.anim.fade_out);
-//        logo1.startAnimation(fade2);
+        pics.startAnimation(rotation);
+        ImageView centerImage = (ImageView) findViewById(R.id.imageView1);
+        centerImage.startAnimation(rotation);
+//        setContentView(R.layout.menu);
+        final Animation fade2 = AnimationUtils.loadAnimation(this,R.anim.fade_out);
+        logo2.startAnimation(fade2);
+        fade2.setAnimationListener(new Animation.AnimationListener() {
+            @Override
+            public void onAnimationStart(Animation animation) {
+            }
+
+            @Override
+            public void onAnimationEnd(Animation animation) {
+                startActivity(new Intent(MainActivity.this,MenuActivity.class));
+                MainActivity.this.finish();
+            }
+
+            @Override
+            public void onAnimationRepeat(Animation animation) {
+            }
+        });
     }
 
-    protected void onPause(){
-        TextView logo2 = (TextView) findViewById(R.id.textView2);
-        Animation fade2 = AnimationUtils.loadAnimation(this,R.anim.fade_out);
-        logo2.startAnimation(fade2);
-    }
+//    @Override
+//    protected void onPause(){
+//        TextView logo2 = (TextView) findViewById(R.id.textView2);
+//        Animation fade2 = AnimationUtils.loadAnimation(this,R.anim.fade_out);
+//        logo2.startAnimation(fade2);
+//    }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
